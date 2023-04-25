54e0a37a9b114e4b4214df4cc55322abdecf4c7b
aigerim0 <bolotovaaigerim42@gmail.com>
2023-04-25 17:15:38 +0300

    first commit

---
+++ src/components/ToDoForm/index.js
@@ -0,0 +1,37 @@
+import {useState} from "react";
+import {useDispatch} from "react-redux";
+import {addTodo} from "../../redux/todoSlice";
+
+const ToDoForm = () => {
+    const [value, setValue] = useState('');
+
+    const dispatch = useDispatch()
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        dispatch(addTodo({
+                title: value,
+            },
+            setValue('')
+        ))
+    };
+
+    return (
+        <form onSubmit={onSubmit} className='form-inline mt-3 mb-3'>
+
+            <input
+                type='text'
+                className='form-control mb-2 mr-sm-2'
+                placeholder='Add todo...'
+                value={value}
+                onChange={(event) => setValue(event.target.value)}
+            />
+
+            <button type='submit' className='btn btn-primary mb-2 d-flex justify-content-center'>
+                Submit
+            </button>
+        </form>
+    );
+};
+
+export default ToDoForm;
